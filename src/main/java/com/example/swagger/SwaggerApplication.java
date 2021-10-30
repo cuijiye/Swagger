180ff73534a8db1fb2b4ff35921b05bf51b7db0d
cuijiye <cuijiye1993@163.com>
2021-10-30 14:45:59 +0800

    开启定时任务

--- src/main/java/com/example/swagger/SwaggerApplication.java
+++ src/main/java/com/example/swagger/SwaggerApplication.java
@@ -3,9 +3,11 @@ package com.example.swagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
+@EnableAsync //开启异步注解功能
+@EnableScheduling //开启定时任务
 public class SwaggerApplication {
 
     public static void main(String[] args) {
