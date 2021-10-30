c1799b879f21c1ba55ed281cfad2795917184a5c
cuijiye <cuijiye1993@163.com>
2021-10-30 15:02:05 +0800

    开启定时任务

--- src/main/java/com/example/swagger/service/ScheduledService.java
+++ src/main/java/com/example/swagger/service/ScheduledService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScheduledService {
 
-    //cron表达式 秒 分 时 日 月 周几
+    //cron表达式        秒 分 时 日 月 周几
     @Scheduled(cron = "30 27 14 * * 0-7")
     public void testTask(){
         System.out.println("测试定时任务执行！");
