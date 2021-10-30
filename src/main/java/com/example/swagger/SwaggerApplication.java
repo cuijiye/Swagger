18b1f2718785eeab665ab1978b0093d6fa998502
cuijiye <cuijiye1993@163.com>
2021-10-30 11:04:56 +0800

    异步任务

--- src/main/java/com/example/swagger/SwaggerApplication.java
+++ src/main/java/com/example/swagger/SwaggerApplication.java
@@ -2,8 +2,10 @@ package com.example.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SwaggerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/swagger/controller/HelloWorld.java
+++ src/main/java/com/example/swagger/controller/HelloWorld.java
@@ -1,13 +1,19 @@
 package com.example.swagger.controller;
 
 import com.example.swagger.pojo.User;
+import com.example.swagger.service.AsyncService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+
+    @Autowired
+    AsyncService asyncService;
 
     //只要我们的返回值中存在实体类，那么实体类也会被扫描到Swagger中
     @ApiOperation("hello测试类")
@@ -15,5 +21,10 @@ public class HelloWorld {
     public User demo(@ApiParam("用户信息") User user){
         user.setUsername(user.getUsername()+"，hello World!!!");
         return user;
+    }
+    @RequestMapping("/asyncTest")
+    public String asyncTest(){
+        asyncService.hello();
+        return "OK";
     }
 }
